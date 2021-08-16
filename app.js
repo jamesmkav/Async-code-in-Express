9efc604b0b4607a498100e1b9fc686321b7012d1
Jim <jamesmkav@gmail.com>
2021-08-16 11:14:52 -0700

    Initial commit

---
+++ app.js
@@ -0,0 +1,76 @@
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+const { render } = require('pug');
+
+const app = express();
+
+app.set('view engine', 'pug');
+app.set("views", path.join(__dirname, "views"));
+app.use(express.static('public'));
+
+function asyncHandler(callback) {
+  return async (req, res, next) => {
+    try {
+      await callback(req, res, next);
+    }
+    catch (err) {
+      res.render('error', { error: err });
+    }
+  }
+}
+
+
+//CALL BACKS
+/* function getUsers(cb){
+  fs.readFile('data.json', 'utf8', (err, data) => {
+    if (err) return cb(err);
+    const users = JSON.parse(data);
+    return cb(null, users);
+  });
+}
+
+app.get('/', (req,res) => {
+  getUsers(() => {
+    if(err) {
+      res.render('error', { error: err });
+    }
+    else {
+      res.render('index', { title: "Users", users: users.users })
+    }
+  });
+});  */
+
+//PROMISES
+function getUsers() {
+  return new Promise((resolve, reject) => {
+    fs.readFile('data.json', 'utf-8', (err, data) => {
+      if(err) {
+        reject(err);
+      }
+      else {
+        const users = JSON.parse(data);
+        resolve(users);
+      }
+    });
+  });
+}
+
+/* app.get('/', (req, res) => {
+  getUseres()
+  .then((users) => {
+    res.render('index', { title: "Users", users: users.users });
+  })
+  .catch((err) => {
+    res.render('error', { error: err });
+  });
+}); */
+
+//Async/Await
+app.get('/', asyncHandler(async (req, res) => {
+    const users = await getUsers();
+    res.render('index', { title: "Users", users: users.users });
+}));
+
+
+app.listen(3000, () => console.log('App listening on port 3000!'));
